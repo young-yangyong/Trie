6a247dbc8facb7ee3f2c63eaab4b8d8556eba63b
YOUNG <YOUNG@YOUNG.com>
2021-06-22 23:44:31 +0800

    字典树

--- src/trie/Test.java
+++ src/trie/Test.java
@@ -1,21 +1,27 @@
 package trie;
 
 
-public class Test{
+public class Test {
     public static void main(String[] args) {
         final Trie trie = new Trie();
         trie.add("abc");
         trie.add("acd");
         trie.add("bdf");
         trie.add("abcde");
+        trie.add("abcdef");
         trie.add("abcfr");
         trie.add("abcbe");
         System.out.println(trie.size());
+        System.out.println(trie.contains("abcd"));
+        System.out.println(trie.hasPrefix("abc"));
         System.out.println(trie.getWordsByPrefix("*"));
+        System.out.println(trie.getWordsByPrefix("b"));
         System.out.println(trie.getWordsByPrefix("B"));
         System.out.println(trie.getWordsByPrefix("a"));
         System.out.println(trie.remove("abcde"));
+        System.out.println(trie.size());
         System.out.println(trie.getWordsByPrefix("*"));
-
+        System.out.println(trie.remove("bdf"));
+        System.out.println(trie.getWordsByPrefix("*"));
     }
 }
--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -6,9 +6,17 @@ import java.util.List;
 import java.util.Map;
 
 public class Trie {
-
+    /*
+    根节点
+     */
     private final Node root;
+    /*
+    字典树包含单词个数
+     */
     private int size;
+    /*
+    回溯删除节点的标志位
+     */
     private int DeleteFlag;
 
 
@@ -59,6 +67,11 @@ public class Trie {
         return node.isWorld;
     }
 
+    /**
+     * 检查trie中前缀prefix是否存在
+     *
+     * @return true表示存在该前缀
+     */
     public boolean hasPrefix(String prefix) {
         if (prefix == null || prefix.length() == 0)
             return false;
@@ -72,11 +85,20 @@ public class Trie {
         return true;
     }
 
+    /**
+     * 以node作为深度优先遍历的起始节点及根节点，遍历以node为根节点的树的所有节点，找到所有单词加入words集合中
+     *
+     * @param words 保存单词的集合
+     * @param node  根节点和遍历起始点
+     */
     private void DFS(List<String> words, Node node) {
         if (node == null)
             return;
         if (node.isWorld)
             words.add(node.value);
+        /*
+        遍历余下所有分支
+         */
         if (node.next != null)
             node.next.forEach((k, v) -> DFS(words, v));
     }
@@ -93,11 +115,13 @@ public class Trie {
         List<String> words = new ArrayList<>();
         if (prefix == null || prefix.length() == 0)
             return words;
-
         /*
         查询所有
          */
         if (prefix.equals("*")) {
+            /*
+            深度优先遍历
+             */
             DFS(words, root);
             return words;
         }
@@ -112,34 +136,59 @@ public class Trie {
         return words;
     }
 
+    /**
+     * 深度优先遍历的变体，用以辅助删除trie中的单词和单词对应的分支
+     *
+     * @return true表示删除成功，未找到该单词会返回false
+     */
     private boolean removeByDFS(Node node, String word, int i) {
-        if (node == null)
+        if (node == null)//递归中未找到单词的某一个节点，说明该单词不存在，直接结束递归
             return false;
-        if (i == word.length()) {
-            if (node.isWorld) {
-                if (!node.next.isEmpty()) {
-                    node.isWorld = false;
+        if (i == word.length()) {//递归到达保存该单词的节点
+            if (node.isWorld) {//判断该节点是否有单词
+                if (!node.next.isEmpty()) {//该节点下仍有单词
+                    node.isWorld = false;//直接置isWorld标志位为false表明已经删除该单词
+                    --size;//维护trie大小
+                } else {//表明该节点是该分支下最终节点，置DeleteFlag=1表明回溯时清理该分支，删除无用节点
+                    DeleteFlag = 1;
                     --size;
-                } else DeleteFlag = 1;
-                return true;
+                }
+                return true;//无论如何都表明已找到该单词，并且要删除该单词
             }
+            /*
+            该单词没有被保存，置DeleteFlag标志位为0，表示回溯时不需要清理分支
+             */
             DeleteFlag = 0;
             return false;
         }
-        if (DeleteFlag == -1)
-            if (removeByDFS(node.next.get(word.charAt(i)), word, i + 1)) {
-                if (DeleteFlag == 1)
-                    if (node.next.size() == 1)
-                        node.next.clear();
-                    else{
-                        node.next.remove(word.charAt(i));
-                        DeleteFlag = 0;
-                    }
-                return true;
+        //if条件成立，进入回溯
+        if (removeByDFS(node.next.get(word.charAt(i)), word, i + 1)) {
+            if (DeleteFlag == 1) {//是否要清理分支
+                /*
+                表示该节点分支超过1个，说明从该节点起有其它单词在使用该节点，不能再删除该节点及以上的节点，
+                删除最后一个分支节点后,直接置DeleteFlag = 0停止回溯中清理分支操作
+                 */
+                if (node.next.size() > 1) {
+                    node.next.remove(word.charAt(i));
+                    DeleteFlag = 0;
+                    return true;
+                }
+                /*
+                确保该分支是一个单分支的情况下，仍然可以成功删除该分支
+                 */
+                if (node.equals(root))
+                    node.next.remove(word.charAt(i));
             }
-        return false;
+            return true;
+        }
+        return false;//if条件不成立，单词不存在，直接回溯false
     }
 
+    /**
+     * 删除trie中的word单词
+     *
+     * @return true表示删除成功，false表示单词不存在
+     */
     public boolean remove(String word) {
         if (word == null || word.length() == 0)
             throw new RuntimeException("非法参数！");
@@ -147,9 +196,24 @@ public class Trie {
         return removeByDFS(root, word, 0);
     }
 
+    /**
+     * trie中的节点类
+     */
     private static class Node {
+        /*
+       保存该节点到根节点构成的字符串，
+       这么做会极其浪费空间，大部分情况下根本没必要保存，这里我为啥使用？为了方便！
+       只要能有高效的时间效率，空间是值得牺牲的，空间是可以扩展的嘛，时间可不等人。。。
+         */
         private final String value;
+        /*
+        是否表示单词
+         */
         boolean isWorld;
+        /*
+        保存分支结点
+        HashMap很nice！
+         */
         Map<Character, Node> next;
 
         public Node(String value, boolean isWorld) {
@@ -162,5 +226,4 @@ public class Trie {
             this(value, false);
         }
     }
-
 }
